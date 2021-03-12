3065117ddbf850c4394ee61fe82d59e05e9f13db
Devanshi182 <devanshi.pathak@adypu.edu.in>
2021-03-12 06:11:31 +0530

    Unity Final Project

---
+++ Devanshi_Final_Assessment1/Assets/Color_Dynamic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color_Dynamic : MonoBehaviour
+{
+    public Material[] materials_group;
+    int random_material;
+    void Start()
+    {
+        //random_material = Random.Range(0, 4);
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = materials_group[Random.Range(0, 4)];
+
+    }
+}
---
+++ Devanshi_Final_Assessment1/Assets/Cube_Scaling_East.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Scaling_South : MonoBehaviour
+{
+    float i = 1;
+    // Start is called before the first frame update
+    public void scaling_cube()
+    {
+        i = i + 1f;
+        transform.localScale = new Vector3(i, 1f, 1f);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Color objColor = GetComponent<Renderer>().material.color;
+        Color obsColor = collision.gameObject.GetComponent<Renderer>().material.color;
+        if (objColor.Equals(obsColor) || collision.gameObject.CompareTag("center_cube"))
+        {
+            i = 1;
+            transform.localScale = new Vector3(1, 1, 1);
+            Destroy(collision.gameObject);
+        }
+    }
+}
+
+
